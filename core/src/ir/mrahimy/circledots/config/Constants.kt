440f45c10adfdcf229a48b8a00c87126bf28a6de
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-27 13:06:46 +0330

    + base constants (copied from paisley)

---
+++ core/src/ir/mrahimy/circledots/config/Constants.kt
@@ -0,0 +1,20 @@
+package ir.mrahimy.circledots.config
+
+object Constants {
+    val GAME_W = 360
+    val GAME_H = 640
+    val WORLD_WIDTH = 225
+    val WORLD_HEIGHT = 400
+    val BALL_MAX_RADIUS = 50
+    val BALL_MIN_RADIUS = 30
+    val ASPECT_RATIO = GAME_W.toFloat() / GAME_H.toFloat()
+
+    val GAME_TITLE = "Xircle"
+    val HudBoxMargin = 5f
+
+    val GRID_WIDTH_STEP = 23
+    val GRID_WIDTH_START = 9
+    val GRID_H_STEP = 20
+    val GRID_H_START = 10
+
+}
