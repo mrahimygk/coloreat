ce329d3ad1850be13c52252fff68267b1ceb4640
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-27 11:50:38 +0330

    initing a basic libgdx app made by gdx-setup.jar in Oct 27 2019 nightly build

---
+++ desktop/src/ir/mrahimy/circledots/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ir.mrahimy.circledots.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ir.mrahimy.circledots.Xircle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Xircle(), config);
+	}
+}
