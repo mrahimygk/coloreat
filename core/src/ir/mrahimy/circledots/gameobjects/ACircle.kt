afc7edc4b93c807faaf7939c20afb16907d7faa8
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-29 14:19:29 +0330

    + a class to load circle texture

---
+++ core/src/ir/mrahimy/circledots/gameobjects/ACircle.kt
@@ -0,0 +1,31 @@
+package ir.mrahimy.circledots.gameobjects
+
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Circle
+import ir.mrahimy.circledots.manage.Assets
+
+class ACircle(private val bounds: Circle) {
+    private val circleSprite = Sprite(Assets().xircleTexture())
+
+    init {
+        circleSprite.setOriginCenter()
+        circleSprite.setPosition(
+                bounds.x - circleSprite.texture.width / 2,
+                bounds.y - circleSprite.texture.height / 2)
+
+        circleSprite.color = Color.WHITE.cpy().lerp(Color.BLUE, .5f)
+    }
+
+    fun render(batch: SpriteBatch, deltaTime: Float) {
+        val s = 1f / ((circleSprite.texture.width / 2) / bounds.radius)
+        circleSprite.setScale(s)
+        circleSprite.setScale(s)
+        circleSprite.setScale(s)
+
+        batch.begin()
+        circleSprite.draw(batch)
+        batch.end()
+    }
+}
