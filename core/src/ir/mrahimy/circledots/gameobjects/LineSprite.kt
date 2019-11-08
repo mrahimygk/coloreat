692d48beea9c97e11f8b7411acc355509112935e
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-11-08 19:08:25 +0330

    drawing texture by the help of sprite batch ktx

--- core/src/ir/mrahimy/circledots/gameobjects/LineSprite.kt
+++ core/src/ir/mrahimy/circledots/gameobjects/LineSprite.kt
@@ -1,31 +1,19 @@
 package ir.mrahimy.circledots.gameobjects
 
 import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.math.Circle
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion
+import com.badlogic.gdx.math.Vector2
+import ir.mrahimy.circledots.ktx.drawLine
 import ir.mrahimy.circledots.manage.Assets
 
-class LineSprite(val bounds: Circle, private val color:Color=Color.BLUE) {
-    private val lineSprite = Sprite(Assets().lineTexture())
-
-    init {
-        lineSprite.setOriginCenter()
-        lineSprite.setPosition(
-                bounds.x - lineSprite.texture.width / 2,
-                bounds.y - lineSprite.texture.height / 2)
-
-        lineSprite.color = Color.WHITE.cpy().lerp(color, .5f)
-    }
+class LineSprite(val start: Vector2, val end: Vector2, private val color: Color = Color.BLUE) {
+    private val texture = Assets().lineTexture().apply { setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear) }
 
     fun render(batch: SpriteBatch, deltaTime: Float) {
-        val s = 1f / ((lineSprite.texture.width / 2) / bounds.radius)
-        lineSprite.setScale(s)
-        lineSprite.setScale(s)
-        lineSprite.setScale(s)
-
         batch.begin()
-        lineSprite.draw(batch)
+        batch.drawLine(start.x, start.y, end.x, end.y, 1f, AtlasRegion(texture, 0, 0, texture.width, texture.height))
         batch.end()
     }
 }
