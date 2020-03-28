c20ab986a7df6980e7c140c690002782a3c48309
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-28 19:01:56 +0430

    +ktx on Circle for setting bounds on input

---
+++ core/src/ir/mrahimy/circledots/ktx/Circle.kt
@@ -0,0 +1,12 @@
+package ir.mrahimy.circledots.ktx
+
+import com.badlogic.gdx.math.Circle
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Vector2
+
+fun Circle.findAngle(pos: Vector2) = MathUtils.atan2(pos.y - y, pos.x - x)
+
+fun Circle.moveToAngle(angle: Float, center: Circle) {
+    set(Circle(center.radius * MathUtils.cos(angle) + center.x,
+            center.radius * MathUtils.sin(angle) + center.y, radius))
+}
