c2a022644ff0e0f6178ef6280d2dfce204e3f0e1
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-28 18:51:20 +0430

    +update to get new positions from input

--- core/src/ir/mrahimy/circledots/gameobjects/LineSprite.kt
+++ core/src/ir/mrahimy/circledots/gameobjects/LineSprite.kt
@@ -11,6 +11,11 @@ import ir.mrahimy.circledots.manage.Assets
 class LineSprite(val start: Vector2, val end: Vector2, private val color: Color = Color.BLUE) {
     private val texture = Assets().lineTexture().apply { setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear) }
 
+    fun update(inStart: Vector2, inEnd: Vector2){
+        start.set(inStart.x, inStart.y)
+        end.set(inEnd.x, inEnd.y)
+    }
+
     fun render(batch: SpriteBatch, deltaTime: Float) {
         batch.begin()
         batch.drawLine(start.x, start.y, end.x, end.y, 1f, AtlasRegion(texture, 0, 0, texture.width, texture.height))
