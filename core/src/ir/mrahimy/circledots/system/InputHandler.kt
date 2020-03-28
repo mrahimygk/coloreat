cf952757961215e35ca64c861d41463b8180a529
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-28 17:48:08 +0430

    +basic moving on touch for the point

--- core/src/ir/mrahimy/circledots/system/InputHandler.kt
+++ core/src/ir/mrahimy/circledots/system/InputHandler.kt
@@ -1,16 +1,33 @@
 package ir.mrahimy.circledots.system
 
 import com.badlogic.gdx.InputProcessor
+import com.badlogic.gdx.math.Circle
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.math.Vector3
+import ir.mrahimy.circledots.gameobjects.PointSprite
 
-class InputHandler(private val gameRenderer: GameRenderer) : InputProcessor {
+class InputHandler(private val gameRenderer: GameRenderer, private val gameWorld: GameWorld) : InputProcessor {
 
     private val touchPos: Vector3 = Vector3()
+    private var movingPoint: PointSprite? = null
+    private var movingPointInitBounds: Circle? = null
+    private val center = Vector2(gameWorld.circleSprite.bounds.x, gameWorld.circleSprite.bounds.y)
 
     override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
         touchPos.set(screenX.toFloat(), screenY.toFloat(), 0f)
         gameRenderer.viewport.unproject(touchPos)
         println(touchPos)
+
+        movingPoint = gameWorld.points.firstOrNull { Circle(touchPos.x, touchPos.y, 15f).contains(it.bounds) }
+                ?: return false
+        movingPoint?.let { point ->
+            //creating a copy of the first position
+            movingPointInitBounds = Circle(point.bounds)
+
+            point.bounds.set(Circle(point.bounds.radius * MathUtils.cos(2.0f) + point.bounds.x,
+            point.bounds.radius * MathUtils.sin(2.0f) + point.bounds.y, 2.5f))
+        }
         return false
     }
 
