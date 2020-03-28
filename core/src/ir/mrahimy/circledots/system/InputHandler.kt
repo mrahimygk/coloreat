4f653a2fe481b78de7876b4dbf91334af610f7f9
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-28 19:03:12 +0430

    using ktx on circle for cleaner code

--- core/src/ir/mrahimy/circledots/system/InputHandler.kt
+++ core/src/ir/mrahimy/circledots/system/InputHandler.kt
@@ -2,14 +2,14 @@ package ir.mrahimy.circledots.system
 
 import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.math.Circle
-import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.math.Vector2
-import ir.mrahimy.circledots.gameobjects.PointSprite
+import ir.mrahimy.circledots.ktx.findAngle
+import ir.mrahimy.circledots.ktx.moveToAngle
 
 class InputHandler(private val gameRenderer: GameRenderer, private val gameWorld: GameWorld) : InputProcessor {
 
     private val touchPos: Vector2 = Vector2()
-    private var movingPoint: PointSprite? = null
+    private var movingPoint: Circle? = null
     private val center = gameWorld.circleSprite.bounds
 
     override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
@@ -17,32 +17,17 @@ class InputHandler(private val gameRenderer: GameRenderer, private val gameWorld
         gameRenderer.viewport.unproject(touchPos)
         println(touchPos)
 
-        movingPoint = gameWorld.points.firstOrNull { Circle(touchPos.x, touchPos.y, 15f).contains(it.bounds) }
+        movingPoint = gameWorld.points.firstOrNull { Circle(touchPos.x, touchPos.y, 15f).contains(it.bounds) }?.bounds
                 ?: return false
-        movingPoint?.let { point ->
-            movePointToAngle(point.bounds, findAngle(Vector2(point.bounds.x, point.bounds.y)))
-        }
+
         return false
-    }
-
-    private fun movePointToAngle(point: Circle, angle: Float) {
-        point.set(Circle(center.radius * MathUtils.cos(angle) + center.x,
-                center.radius * MathUtils.sin(angle) + center.y, point.radius))
-        gameWorld.updateLines()
-    }
-
-    private fun findAngle(touchPos: Vector2): Float {
-        val diffX = touchPos.x - center.x
-        val diffY = touchPos.y - center.y
-        return MathUtils.atan2(diffY, diffX)
     }
 
     override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
         touchPos.set(screenX.toFloat(), screenY.toFloat())
         gameRenderer.viewport.unproject(touchPos)
-        movingPoint?.let { point ->
-            movePointToAngle(point.bounds, findAngle(touchPos))
-        }
+        movingPoint?.moveToAngle(center.findAngle(touchPos), center)
+        gameWorld.updateLines()
         return false
     }
 
