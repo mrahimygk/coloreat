d2637b09f6d0b7876917f27256f73d7f72ba23ac
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-03-29 07:55:32 +0430

    +setter for color wrapper

--- core/src/ir/mrahimy/circledots/gameobjects/PointSprite.kt
+++ core/src/ir/mrahimy/circledots/gameobjects/PointSprite.kt
@@ -4,14 +4,25 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Circle
+import com.badlogic.gdx.math.Intersector
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Vector2
 import ir.mrahimy.circledots.manage.Assets
 
-class PointSprite(val bounds: Circle, private val color: Color = Color.RED) {
+class PointSprite(val bounds: Circle, color: Color = Color.RED, private val isCenter: Boolean = false) {
     private val pointSprite = Sprite(Assets().pointTexture())
+    private val inactiveColor = color
 
     init {
         update()
+        pointSprite.color = inactiveColor
     }
+
+    var color: Color = color
+        set(value) {
+            field = value
+            pointSprite.color = field
+        }
 
     fun render(batch: SpriteBatch, deltaTime: Float) {
         update()
@@ -25,12 +36,21 @@ class PointSprite(val bounds: Circle, private val color: Color = Color.RED) {
         batch.end()
     }
 
-    private fun update(){
+    private fun update() {
         pointSprite.setOriginCenter()
         pointSprite.setPosition(
                 bounds.x - pointSprite.texture.width / 2,
                 bounds.y - pointSprite.texture.height / 2)
 
-        pointSprite.color = Color.WHITE.cpy().lerp(color, .5f)
+    }
+
+    fun updateStatus(points: List<Vector2>) {
+        if (!isCenter) return
+        if (Intersector.isPointInTriangle(Vector2(bounds.x, bounds.y),
+                        points[0], points[1], points[2])) {
+            color = Color.GREEN//.cpy().lerp(color, .5f)
+        } else {
+            color = inactiveColor
+        }
     }
 }
