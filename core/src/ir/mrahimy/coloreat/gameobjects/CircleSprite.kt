b519e57d3eca0f7ec49e56f6fe9cd1af42042d99
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-03 11:46:30 +0430

    +another sprite for filled circle

--- core/src/ir/mrahimy/coloreat/gameobjects/CircleSprite.kt
+++ core/src/ir/mrahimy/coloreat/gameobjects/CircleSprite.kt
@@ -5,36 +5,43 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Circle
+import ir.mrahimy.coloreat.ktx.scale
 import ir.mrahimy.coloreat.manage.Assets
 
-class CircleSprite(val bounds: Circle, color: Color = Color.BLACK) {
+class CircleSprite(val bounds: Circle, color: Color = Color.WHITE) {
     private val circleSprite = Sprite(Assets().circleTexture().apply { setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear) })
+    private val pointSprite = Sprite(Assets().pointTexture())
 
     init {
         update()
+        circleSprite.color = Color.BLACK
     }
 
     var color: Color = color
         set(value) {
             field = value
-            circleSprite.color = field
+            pointSprite.color = field
         }
 
-    fun update(){
+    fun update() {
         circleSprite.setOriginCenter()
         circleSprite.setPosition(
                 bounds.x - circleSprite.texture.width / 2,
                 bounds.y - circleSprite.texture.height / 2)
+
+        pointSprite.setOriginCenter()
+        pointSprite.setPosition(
+                bounds.x - pointSprite.texture.width / 2,
+                bounds.y - pointSprite.texture.height / 2)
     }
 
     fun render(batch: SpriteBatch, deltaTime: Float) {
         update()
-        val s = 1f / ((circleSprite.texture.width / 2) / bounds.radius)
-        circleSprite.setScale(s)
-        circleSprite.setScale(s)
-        circleSprite.setScale(s)
+        pointSprite.scale(bounds)
+        circleSprite.scale(bounds)
 
         batch.begin()
+        pointSprite.draw(batch)
         circleSprite.draw(batch)
         batch.end()
     }
