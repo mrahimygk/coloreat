d886a91f4c2e374536f01601d4ea19c07f27b71b
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-03 10:27:57 +0430

    fixing renders, bg to white

--- core/src/ir/mrahimy/coloreat/system/GameRenderer.kt
+++ core/src/ir/mrahimy/coloreat/system/GameRenderer.kt
@@ -4,7 +4,7 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import ir.mrahimy.coloreat.screen.FitScreenImpl
 
-class GameRenderer(game: Xircle, private val world: GameWorld) : FitScreenImpl(game, Color(.0f, .0f, .0f, 1f)) {
+class GameRenderer(game: Xircle, private val world: GameWorld) : FitScreenImpl(game, Color(1.0f, 1.0f, 1.0f, 1f)) {
 
     private val batch = SpriteBatch()
 
@@ -16,25 +16,23 @@ class GameRenderer(game: Xircle, private val world: GameWorld) : FitScreenImpl(g
         super.render(0f)
         renderCircle(delta)
         renderLines(delta)
-        renderPoints(delta)
+        renderPlayer(delta)
     }
 
     private fun renderCircle(delta: Float) {
-        world.circleSprite.render(batch, delta)
-        world.centerPoint.render(batch, delta)
+//        world.circleSprite.render(batch, delta)
+//        world.centerPoint.render(batch, delta)
     }
 
-    private fun renderPoints(delta: Float) {
-        world.points.forEach {
-            it.render(batch, delta)
-        }
+    private fun renderPlayer(delta: Float) {
+        world.player.render(batch, delta)
     }
 
     private fun renderLines(delta: Float) {
-        world.linePoints.forEach {
-            world.lineSprite.update(it.first, it.second)
-            world.lineSprite.render(batch, delta)
-        }
+//        world.linePoints.forEach {
+//            world.lineSprite.update(it.first, it.second)
+//            world.lineSprite.render(batch, delta)
+//        }
     }
 
 }
