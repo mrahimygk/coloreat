971a92b106a2af3967b36d21164eb45d996ec21d
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2020-04-03 10:30:18 +0430

    adding circle update

--- core/src/ir/mrahimy/coloreat/gameobjects/CircleSprite.kt
+++ core/src/ir/mrahimy/coloreat/gameobjects/CircleSprite.kt
@@ -7,19 +7,28 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Circle
 import ir.mrahimy.coloreat.manage.Assets
 
-class CircleSprite(val bounds: Circle, private val color: Color = Color.BLUE) {
+class CircleSprite(val bounds: Circle, color: Color = Color.BLACK) {
     private val circleSprite = Sprite(Assets().circleTexture().apply { setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear) })
 
     init {
+        update()
+    }
+
+    var color: Color = color
+        set(value) {
+            field = value
+            circleSprite.color = field
+        }
+
+    fun update(){
         circleSprite.setOriginCenter()
         circleSprite.setPosition(
                 bounds.x - circleSprite.texture.width / 2,
                 bounds.y - circleSprite.texture.height / 2)
-
-        circleSprite.color = Color.WHITE.cpy().lerp(color, .5f)
     }
 
     fun render(batch: SpriteBatch, deltaTime: Float) {
+        update()
         val s = 1f / ((circleSprite.texture.width / 2) / bounds.radius)
         circleSprite.setScale(s)
         circleSprite.setScale(s)
